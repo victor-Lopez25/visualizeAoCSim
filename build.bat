83c3536b003b1098212d0392a0e39e982936992d
victhor <victorlopezcortes25@gmail.com>
2024-12-06 23:00:07 +0100

    changed build to release

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 
 set opts=-debug -vet -vet-using-param
+set releaseopts=-no-bounds-check -no-type-assert -o:speed
 
-odin build %cd% %opts%
-day6visualize.exe
+odin build %cd% %releaseopts%
+visualizeAoCSim.exe
